aef63571627ceb1f2003d9045174e5c5ee66490c
Fitz <andrew.fitz@fresh8gaming.com>
2022-03-07 18:56:22 +0000

    set up work load identity onfor test app and give cloud sql permission

--- tf-faceit/test-app/service-account.tf
+++ tf-faceit/test-app/service-account.tf
@@ -3,14 +3,14 @@ resource "google_service_account" "test_app" {
   display_name = "Test App"
 }
 
-# resource "google_service_account_iam_member" "test_app" {
-#   service_account_id = google_service_account.test_app.name
-#   role               = "roles/iam.workloadIdentityUser"
-#   member             = "serviceAccount:${var.faceit_project_id}.svc.id.goog[${kubernetes_namespace.test_app.metadata[0].name}/test-app]"
-# }
+resource "google_service_account_iam_member" "test_app" {
+  service_account_id = google_service_account.test_app.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${var.faceit_project_id}.svc.id.goog[${kubernetes_namespace.test_app.metadata[0].name}/test-app]"
+}
 
-# resource "google_project_iam_member" "test_app" {
-#   project = var.faceit_project_id
-#   role    = "roles/cloudsql.client"
-#   member  = "serviceAccount:${google_service_account.test_app.email}"
-# }
+resource "google_project_iam_member" "test_app" {
+  project = var.faceit_project_id
+  role    = "roles/cloudsql.client"
+  member  = "serviceAccount:${google_service_account.test_app.email}"
+}
