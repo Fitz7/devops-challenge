41c4194c4ffdbc35c0a81bd31f42ce2b63979eda
Fitz <andrew.fitz@fresh8gaming.com>
2022-03-07 16:32:31 +0000

    add sqladmin.googleapis.com  to project services

--- tf-faceit/gcp/project.tf
+++ tf-faceit/gcp/project.tf
@@ -8,7 +8,8 @@ resource "google_project_service" "service" {
     "artifactregistry.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "serviceusage.googleapis.com",
-    "servicenetworking.googleapis.com"
+    "servicenetworking.googleapis.com",
+    "sqladmin.googleapis.com"
   ])
 
   service = each.key
